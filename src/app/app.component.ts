b85310e57a62e18b78c287cf6275fe26ec726294
Victor Chabbert <chabbertvi@eisti.eu>
2017-03-25 12:35:41 +0100

    Refactor app component to a bare minimum

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'ls-root',
+  template: `<router-outlet></router-outlet>`,
 })
-export class AppComponent {
-  title = 'app works!';
-}
+export class AppComponent {}
