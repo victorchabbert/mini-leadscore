bdb48e4d76316cbdfdee34e4d744995b3f1cc61f
Victor Chabbert <chabbertvi@eisti.eu>
2017-03-25 18:19:21 +0100

    Create contacts table with mock data

--- src/app/dashboard/contacts/contacts.component.ts
+++ src/app/dashboard/contacts/contacts.component.ts
@@ -3,12 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ls-contacts',
   templateUrl: './contacts.component.html',
+  styles: [`
+    .ls-fmw {
+      flex: 1 0 100px;
+    }
+  `]
 })
 export class ContactsComponent implements OnInit {
-
+  contacts = [];
   constructor() { }
 
   ngOnInit() {
+    this.fetch(data => this.contacts = data);
+  }
+
+  fetch(cb) {
+    const req = new XMLHttpRequest();
+    req.open('GET', 'assets/MOCK_DATA.json');
+    req.onload = () => cb(JSON.parse(req.response));
+    req.send();
   }
 
 }
