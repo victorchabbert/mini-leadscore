270345002d788f7550aee58e9503eada0b32700a
Victor Chabbert <chabbertvi@eisti.eu>
2017-03-25 14:55:22 +0100

    Add dashboard sidebar

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ls-dashboard',
   templateUrl: './dashboard.component.html',
-  styles: []
 })
 export class DashboardComponent implements OnInit {
 
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
     SharedModule,
+    RouterModule,
   ],
   declarations: [DashboardComponent],
   exports: [
